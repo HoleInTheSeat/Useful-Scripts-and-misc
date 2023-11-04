79ee1fddc802d0e6d885b0ad5142b5bf5a9d8556
HoleInTheSeat <58385663+HoleInTheSeat@users.noreply.github.com>
2023-11-04 01:31:38 -0500

    Update ACME-LETSENCRYPT-CLOUDFLARE_DNS.sh

--- PacketFence/ACME-LETSENCRYPT-CLOUDFLARE_DNS.sh
+++ PacketFence/ACME-LETSENCRYPT-CLOUDFLARE_DNS.sh
@@ -3,13 +3,13 @@
 # run certbot (checks for existing cert and renews if needed)
 certbot certonly --dns-cloudflare --dns-cloudflare-credentials /home/packetfence/.secrets/cloudflare.ini -d <Admin WebGUI Domain> -d <Portal Domain> --preferred-challenges dns-01 --non-interactive --expand
 
-# backup old cert (honestly no reason too)
-cp /usr/local/pf/conf/ssl/server.pem /usr/local/pf/conf/ssl/server--OLD.pem
-
 # copy in new cert
-mv /usr/local/pf/conf/ssl/server.crt /usr/local/pf/conf/ssl/server.crt.old
-cat /etc/letsencrypt/live/<DOMAIN>/fullchain.pem > /usr/local/pf/conf/ssl/server.cert
-cat /etc/letsencrypt/live/<DOMAIN>/privkey.pem > /usr/local/pf/conf/ssl/server.key
+cat /etc/letsencrypt/live/packetfence.armchairsavages.net-0001/fullchain.pem > /usr/local/pf/conf/ssl/server.crt
+cat /etc/letsencrypt/live/packetfence.armchairsavages.net-0001/privkey.pem > /usr/local/pf/conf/ssl/server.key
+
+# set cert perms
+chown packetfence:packetfence /usr/local/pf/conf/ssl/server.crt
+chown packetfence:packetfence /usr/local/pf/conf/ssl/server.key
 
 # Restart HAPROXY
 /usr/local/pf/bin/pfcmd service haproxy-admin restart
