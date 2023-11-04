224422f434675a456ebf5a47242e541df1f7ad4b
HoleInTheSeat <58385663+HoleInTheSeat@users.noreply.github.com>
2023-11-04 02:43:53 -0500

    Update ACME-LETSENCRYPT-CLOUDFLARE_DNS.sh

--- PacketFence/ACME-LETSENCRYPT-CLOUDFLARE_DNS.sh
+++ PacketFence/ACME-LETSENCRYPT-CLOUDFLARE_DNS.sh
@@ -4,14 +4,10 @@
 certbot certonly --dns-cloudflare --dns-cloudflare-credentials /home/packetfence/.secrets/cloudflare.ini -d <Admin WebGUI Domain> -d <Portal Domain> --preferred-challenges dns-01 --non-interactive --expand
 
 # copy in new cert
-cat /etc/letsencrypt/live/packetfence.armchairsavages.net-0001/fullchain.pem > /usr/local/pf/conf/ssl/server.crt
-cat /etc/letsencrypt/live/packetfence.armchairsavages.net-0001/cert.pem > /usr/local/pf/conf/ssl/server.pem
-cat /etc/letsencrypt/live/packetfence.armchairsavages.net-0001/privkey.pem > /usr/local/pf/conf/ssl/server.key
-
-# set cert perms
-chown packetfence:packetfence /usr/local/pf/conf/ssl/server.crt
-chown packetfence:packetfence /usr/local/pf/conf/ssl/server.pem
-chown packetfence:packetfence /usr/local/pf/conf/ssl/server.key
+cat /etc/letsencrypt/live/packetfence.armchairsavages.net/fullchain.pem > /usr/local/pf/conf/ssl/server.crt
+cat /etc/letsencrypt/live/packetfence.armchairsavages.net/fullchain.pem > /usr/local/pf/conf/ssl/server.pem
+cat /etc/letsencrypt/live/packetfence.armchairsavages.net/privkey.pem > /usr/local/pf/conf/ssl/server.pem
+cat /etc/letsencrypt/live/packetfence.armchairsavages.net/privkey.pem > /usr/local/pf/conf/ssl/server.key
 
 # Restart HAPROXY
 /usr/local/pf/bin/pfcmd service haproxy-admin restart
